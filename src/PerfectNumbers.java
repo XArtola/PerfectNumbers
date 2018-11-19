ce9e8c579ef19061dc308bdd8ef7d89c559e51b4
ik013043z1 <ik013043z1@H013043.NET>
2018-11-19 11:24:01 +0100

    1.0

---
+++ src/PerfectNumbers.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ik013043z1
+ *
+ */
+public class PerfectNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
