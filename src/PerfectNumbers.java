4aa98c544fb8c1db056fbb2002f84408b1ec271c
XArtola <xartolaza18dw@ikzubirimanteo.com>
2018-11-19 17:09:54 +0100

    3.0

--- src/PerfectNumbers.java
+++ src/PerfectNumbers.java
@@ -1,7 +1,10 @@
+
 /**
  * 
  */
 import java.util.Scanner;
+import java.util.ArrayList;
+
 /**
  * @author ik013043z1
  *
@@ -13,29 +16,95 @@ public class PerfectNumbers {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter 10 numbers");
-		
-		int a = 360;
-		int b = 2;
-		
-		while (a >1) {
-			
-			while(a % b != 0) {
-				
-				b++;
-				
+
+		System.out.println("Enter 10 different numbers");
+
+		String number = "";
+		int numberInt = 0;
+
+		int differentNumbers = 0;
+
+		ArrayList<Integer> numberStore = new ArrayList<Integer>();
+
+		while (numberStore.size() < 10) {
+
+			number = sc.next();
+			number = number.trim();
+
+			if (!(number.matches("^[0-9]+"))) {
+
+				System.out.println("This is not a valid number try again.\n You need " + (10 - numberStore.size())
+						+ " more numbers");
 			}
 			
-			a = a/b;
-			System.out.println(b);
+			else if (numberStore.size()==0) {
+				
+				numberStore.add(Integer.parseInt(number));
+	
+			}
+
+			else {
+
+				numberInt = Integer.parseInt(number);
+				System.out.println(numberInt);
+
+				boolean repeated = false;
+
+				for (int a = 0; a < numberStore.size(); a++) {
+
+					if (numberStore.get(a) == numberInt) {
+
+						System.out.println("This is number is already stored try again.\nYou need "
+								+ (10 - differentNumbers) + " more numbers");
+
+						repeated = true;
+
+					}
+
+				}
+
+				if (!repeated) {
+
+					numberStore.add(numberInt);
+
+				}
+
+			}
 			
+			System.out.println("Enter the next number you need " + (10 - numberStore.size()) + " more");
+
+		}
+
+		for (int b = 0; b < numberStore.size(); b++) {
+
+			int sum = 0;
+
+			for (int c = numberStore.get(b).intValue() - 1; c > 0; c--) {
+
+				if (numberStore.get(b).intValue() % c == 0) {
+
+					sum = sum + c;
+
+				}
+
+			}
+
+			if (numberStore.get(b).intValue() == sum) {
+
+				System.out.println(numberStore.get(b).intValue() + " is a perfect number");
+				numberStore.remove(b);
+
+			}
+
 		}
 		
-		System.out.println(a);	
-		
+		if(numberStore.size()==10) {
+			
+			System.out.println("There is not any perfect number");
+		}
+
 	}
 
 }
