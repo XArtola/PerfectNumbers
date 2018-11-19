da3c11c9f8ca0683c831ba1c50f1d0a1c729e814
ik013043z1 <ik013043z1@H013043.NET>
2018-11-19 11:37:42 +0100

    2.0

--- src/PerfectNumbers.java
+++ src/PerfectNumbers.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-
+import java.util.Scanner;
 /**
  * @author ik013043z1
  *
@@ -13,7 +13,29 @@ public class PerfectNumbers {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter 10 numbers");
+		
+		int a = 360;
+		int b = 2;
+		
+		while (a >1) {
+			
+			while(a % b != 0) {
+				
+				b++;
+				
+			}
+			
+			a = a/b;
+			System.out.println(b);
+			
+		}
+		
+		System.out.println(a);	
+		
 	}
 
 }
